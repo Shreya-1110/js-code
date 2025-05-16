8c47454fc3819ac192629722f352f58a4d654b6c
Shreya-1110 <147168561+Shreya-1110@users.noreply.github.com>
2025-05-16 18:03:02 +0000

    all notes basics

--- basics_01/test.js
+++ basics_01/test.js
@@ -1,1 +1,38 @@
-console.log("shreya")
+const name = "shreya"
+const salary = new String ('60lpa')
+
+// console.log( `my name is ${name} and i have a salary of ${salary}`);
+// console.log(salary[3]);
+// console.log(salary.__proto__);
+// console.log(salary.toUpperCase());
+const url = "https://shreya.com/shreya%20games"
+// console.log(url.replace('%20','-'))
+
+// const newString = salary
+// console.log(newString.substring(0,4))
+
+// console.log(Temporal.Now.instant());
+
+let mydate =  new Date()
+// console.log(mydate.toLocaleDateString());
+
+let mycreatedDate = new Date("10-11-2025")
+// console.log(mycreatedDate.toDateString());
+
+let mytimestamp = Date.now()
+// console.log(mycreatedDate.getTime());
+// console.log(Math.floor(Date.now()/1000))
+// console.log(Math.floor(mydate/1000));
+
+let newDate = new Date()
+console.log(newDate.toLocaleString('default'{weekday:"long"}))
+
+
+
+
+
+
+
+
+
+
